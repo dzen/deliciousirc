79bc84c4b2b2d5c5d667a0f549c26482ef9b44d2
Benoit C <dzentoo@gmail.com>
2009-01-30 19:46:55 +0100

    configuration, readme, status, change le title si pas existe.

--- irclicious.py
+++ irclicious.py
@@ -28,6 +28,20 @@ import deliciousapi
 
 from deliciousapi import DeliciousAPI
 
+def getpagetitle(url):
+    import urllib2
+    try:
+        furl = urllib2.urlopen(url)
+        contenu = furl.read()
+        letitre = titre.findall(contenu)
+        if letitre:
+            return letitre[0]
+        else:
+            return None
+    except:
+        return None
+
+
 class PatternList(object):
     """
     A Patternlist is a list of regular expressions. the 'in' operator
@@ -114,9 +128,9 @@ def buildlistfromfile(infile, verbose=True):
 
 class UrlWidget(urwid.WidgetWrap):
     """URL Widget for urwid"""
-    def __init__(self, item, size, unfocused, focused=None, dh=None):
-        self.dh = dh
+    def __init__(self, item, size, unfocused, focused=None):
         self.url = item['url']
+        self.title = None
         self.tags = item['tags']
         self.lutin = self.tags[0]
         self.scrapped = False
@@ -127,19 +141,41 @@ class UrlWidget(urwid.WidgetWrap):
         self.widget = w
         w = urwid.AttrWrap(w, unfocused, focused)
         urwid.WidgetWrap.__init__(self, w)
+        self.size = size
 
     def keypress(self, pos, key):
         return key
 
     def selectable(self):
-
         return True
 
     def post(self):
-        """post this item at loliciouz"""
+        """sets tposted"""
+        self.posted = not self.posted
+        self.widget.set_text('%1s%1s %s' % (\
+                'p'*self.scrapped,
+                'p'*self.posted,
+                 self.url[:self.size[0]-3])
+        )
 
     def retag(self):
-        pass
+        """sets taggued"""
+        self.scrapped = not self.scrapped
+        self.widget.set_text('%1s%1s %s' % (\
+                's'*self.scrapped,
+                'p'*self.posted,
+                 self.url[:self.size[0]-3])
+        )
+
+    def scrap(self):
+        """sets scrapped"""
+        self.scrapped = not self.scrapped
+        self.widget.set_text('%1s%1s %s' % (\
+                's'*self.scrapped,
+                'p'*self.posted,
+                 self.url[:self.size[0]-3])
+        )
+
 
 class MainWindow(object):
     """Our main window"""
@@ -153,8 +189,10 @@ class MainWindow(object):
                 conf.get('delicious', 'login'),
                 conf.get('delicious', 'pass'))
 
+        self.scraplist = []
         self.view = None
 
+# {{{ def: build
     def build(self):
         """build the widgetz"""
         self.ui.register_palette([
@@ -163,21 +201,25 @@ class MainWindow(object):
                 ('header', 'yellow', 'black', 'standout'),
         ])
         self.ui.run_wrapper(self.run)
+# }}}
 
+# {{{ def: redisplay
     def redisplay(self):
         """if the display iz br0kmut"""
         self.ui.draw_screen(self.size, self.view.render(self.size, focus=1))
+# }}}
 
+# {{{ def: run
     def run(self):
         """here we startz"""
         print "started"
         self.size = self.ui.get_cols_rows()
-        itemlist = [UrlWidget(line,self.size,None,'selected', self.dh) for line in urls]
+        itemlist = [UrlWidget(line, self.size, None,'selected') for line in urls]
         self.items = urwid.PollingListWalker(itemlist)
         self.listbox = urwid.ListBox(self.items)
-        self.instruct = urwid.Text("[v]iew [r]etag [p]ost [s]crap [q]quit")
+        self.instruct = urwid.Text("[v]iew [r]etag [i]nfos [p]ost [s]crap [q]quit")
         self.header = urwid.AttrWrap(self.instruct, 'header')
-        self.tagedit = urwid.Edit("Edit teh tag:")
+        self.tagedit = urwid.Edit("")
         self.footer = urwid.AttrWrap(self.tagedit,'header')
         self.view = urwid.Frame(self.listbox, self.header, self.footer)
 
@@ -191,55 +233,104 @@ class MainWindow(object):
             for k in keys:
                 if k in ('q','Q'):
                     # quit
-                    crapfile = open('crap.txt','a')
+                    crapfile = open(self.conf.get('scraplist', 'a+'))
                     crapfile.writelines(scraplist)
                     crapfile.close()
                     return
+
                 if k in ('s','S'):
                     # scrap
-                    #scrap(focus, scraplist)
-                    pass
+                    self.scrap(focus, self.conf.get('scraplist'))
+
+                if k in ('i','I'):
+                    self.display_url_info(focus)
+
                 if k in ('r','R','enter'):
                     self.retag(focus)
-#                if k in ('p','P'):
-#                    self.dlcpost()
+
+                if k in ('p','P'):
+                    self.post(focus)
+
                 if k in ('v','V'):
                     # view
                     webbrowser.open_new_tab(focus.url)
                     self.ui.clear()
                 if "window resize" in keys:
+
                     self.size = self.ui.get_cols_rows()
                     self.redisplay()
                 self.view.keypress(self.size, k)
+# }}}
 
+# {{{ def: scrap
     def scrap(self, item, liste):
         """adds an item to teh scapz list"""
-        item.scrapped = not item.scrapped
-        item.widget.set_text('%1s%1s %s' % ('s'*item.scrapped,
-                                            'p'*item.posted,
-                                            item.url[:size[0]-3]))
+        item.scrap()
+
         domain = urlparse(item.url).netloc
         domain = re.escape(domain)
         domain = '%s\n' % domain
         if item.scrapped:
-            liste.append(domain)
+            self.scraplist.append(domain)
         else:
-            liste.remove(domain)
+            self.scraplist.remove(domain)
+# }}}
+
+# {{{ def: post
+    def post(self, item):
+        """post teh item at delicious"""
+#    def posts_add(self, url, description, extended="", tags="", dt="",
+#            replace="no", shared="yes", **kwds):
+#        """Add a post to del.icio.us. Returns a `result` message or raises an
+#        ``DeliciousError``. See ``self.request()``.
+#
+#        &url (required)
+#            the url of the item.
+#        &description (required)
+#            the description of the item.
+#        &extended (optional)
+#            notes for the item.
+#        &tags (optional)
+#            tags for the item (space delimited).
+#        &dt (optional)
+#            datestamp of the item (format "CCYY-MM-DDThh:mm:ssZ")."""
+        if not item.title:
+            item.title = getpagetitle(item.url) or self.inputwidget("title:")
+#        self.dh.post(item.url, title, tags = ' '.join(item.tags))
+        item.post()
+
+# }}}
+
+# {{{ def get_url_info
+
+    def display_url_info(self, item):
+        """shows the url infos in ``statusbar''"""
+
+
+
+    def inputwidget(self, caption):
+        """returns a text entered"""
+        self.view.set_focus('footer')
+        self.tagedit.set_edit_pos(len(self.tagedit.edit_text))
+        self.tagedit.set_text(caption)
+        while True:
+            keys = self.ui.get_input()
+            self.redisplay()
+            for k in keys:
+                if k == "enter":
+                    self.view.set_focus('body')
+                    ret = self.tagedit.edit_text
+                    self.tagedit.edit_text = ""
+                    self.redisplay()
+                    return ret
+                self.view.keypress(self.size, k)
+
 
     def retag(self, item):
         """retags teh current url"""
         self.view.set_focus('footer')
-        self.tagedit.set_edit_pos(len(self.tagedit.edit_text))
-        while True:
-            keys = self.ui.get_input()
-            canvas = self.view.render(self.size, focus=True)
-            self.ui.draw_screen(self.size, canvas )
-            for k in keys:
-                if k == "enter":
-                    self.view.set_focus('body')
-                    item.tags = [s.strip() for s in self.tagedit.edit_text.split(',')]
-                    return
-                self.view.keypress(self.size, k)
+        tag = self.inputwidget('tags:').strip()
+        item.tags = [s.strip() for s in tag.split(',')]
 
 
 #def run():
